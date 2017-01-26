6b29db0824816453df31fc2694b2235dbfe1421b
Dan Timothy Baconga <danbaconga180@gmail.com>
2017-01-26 13:29:47 +0800

    Add files via upload

---
+++ project3io.java
@@ -0,0 +1,11 @@
+class projectt3io{
+	public static void main(String[]args){
+		for(String d: args){
+			System.out.println("Hello " + d );
+		}
+		System.out.println("There are " + args.length + "Human");
+		System.out.println("Hello Philippines!");
+		System.out.println("I'm Dan Timothy Baconga :)");
+		System.out.println("Have a Blessed day!");
+	}
+}
